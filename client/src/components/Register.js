f7f9c8b76772a2805153c74c6b92e0b67e9eae3e
Richard Tae <taerichard@gmail.com>
2019-01-21 03:48:01 -0800

    established connection from register component to register route, need to create login component witconnecting with db

---
+++ client/src/components/Register.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Icon, Input } from "semantic-ui-react";
+
+class Register extends Component {
+  state = { username: "", password: "", email: "" };
+  onFormSubmit = event => {
+    console.log("submitting");
+    event.preventDefault();
+    this.props.onSubmit(
+      this.state.username,
+      this.state.password,
+      this.state.email
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <Input
+            icon
+            onChange={e => this.setState({ username: e.target.value })}
+            value={this.state.username}
+            placeholder="Username"
+          />
+          <div>
+            <Input
+              icon
+              type="password"
+              onChange={e => this.setState({ password: e.target.value })}
+              value={this.state.password}
+            />
+            <Input icon placeholder="email">
+              <Icon name="mail" />
+              <input
+                onChange={e => this.setState({ email: e.target.value })}
+                value={this.state.email}
+              />
+            </Input>
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
