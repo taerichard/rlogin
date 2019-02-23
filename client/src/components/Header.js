c941449dae52c9b75fb2e954734a3ab7fff2b0d1
Richard Tae <taerichard@gmail.com>
2019-02-22 19:22:02 -0800

    handling home click on main page to home

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item">
-        something
+        <button>Home</button>
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
-          right menu
+          <button>Contact</button>
         </Link>
       </div>
     </div>
